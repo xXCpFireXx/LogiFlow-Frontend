e483b9e1e991efa60c5bf6f52682ac06f1bcb92c
Militaseeee <camilitaacosta2001@gmail.com>
2026-01-12 09:38:26 -0500

    feat: The sidebar design is ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Header } from './components/header/header';
+import { SideBar } from './components/side-bar/side-bar';
+import { Tracking } from './pages/tracking/tracking'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'header',
+    component: Header,
+  },
+  {
+    path: '',
+    component: SideBar,
+  },
+  {
+    path: 'tracking',
+    component: Tracking,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
