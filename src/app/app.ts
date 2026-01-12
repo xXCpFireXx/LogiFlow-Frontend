22c6ea051a9aeb28e365d51247c06acb5c1bd0ac
Militaseeee <camilitaacosta2001@gmail.com>
2026-01-12 16:08:13 -0500

    feat: Responsive sidebar and header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from './components/side-bar/side-bar';
 import { Header } from './components/header/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,11 @@ import { Header } from './components/header/header';
 })
 export class App {
   protected readonly title = signal('LogiFlow-Frontend');
+
+  // Variable para controlar si el sidebar está abierto en móvil
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
--- src/app/components/side-bar/side-bar.ts
+++ src/app/components/side-bar/side-bar.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css',
 })
-export class SideBar {}
+export class SideBar {
+  @Input() isOpen: boolean = false; // Recibe el estado del padre
+  @Output() closeSideBar = new EventEmitter<void>(); // Avisa al padre para cerrar
+}
