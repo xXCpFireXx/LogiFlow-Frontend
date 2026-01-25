dcb9701a62662a757fe861bacfd812b3fa67f430
Militaseeee <camilitaacosta2001@gmail.com>
2026-01-24 23:48:02 -0500

    feat: add user in header and first part to add user in profile

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,10 +1,11 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserProfileNav } from '../../../shared/user-profile-nav/user-profile-nav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [UserProfileNav],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
