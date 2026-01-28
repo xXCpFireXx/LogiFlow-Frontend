4527250cdd5f68676584a01214abbe47e521ea67
SebitasDown <areizasebas31@gmail.com>
2026-01-28 11:22:50 -0500

    fix: Logout

--- src/app/core/layout/side-bar/side-bar.ts
+++ src/app/core/layout/side-bar/side-bar.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../auth.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -13,7 +14,16 @@ export class SideBar {
   @Input() isOpen: boolean = false; // Recibe el estado del padre
   @Output() closeSideBar = new EventEmitter<void>(); // Avisa al padre para cerrar
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   onNavigate() {
+    this.closeSideBar.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
     this.closeSideBar.emit();
   }
 }
